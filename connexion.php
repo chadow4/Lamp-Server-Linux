74471e32e9232e2eaf3c968801d0e3bbf7e23ae8
“Julien <julien.sanchez2@etu.univ-lyon1.fr>
2021-04-10 23:58:20 +0200

    ajout des fichiers connexion.php,info.php et commandes pour installer un serveur web sur un vps linux

---
+++ connexion.php
@@ -0,0 +1,26 @@
+<DOCTYPE html>
+<html>
+        <head>
+                <title>TEST CONNEXION PHP</title>
+                <meta charset="utf-8">
+        </head>
+        <body>
+                <h1>Ma base de données MySQL</h1>
+                <?php
+                        $servername='localhost';
+                        $username='root';
+                        $password='mot_de_passe_de_votre_compte_root';
+                        $conn = new mysqli($servername,$username,$password);
+
+                        if($conn->connect_error){
+                                die('Erreur: ' .$conn->connect_error);
+
+                        }
+                        echo 'Connexion reussie';
+                ?>
+                
+
+                
+        
+        </body>
+</html>
